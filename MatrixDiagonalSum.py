79781a79fa469df101963158ae9eb472ff1e878c
Ali H. Dulaimi <43238519+Decoder3-14@users.noreply.github.com>
2020-12-17 18:41:03 +0300

    Create MatrixDiagonalSum.py

---
+++ MatrixDiagonalSum.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    n = len(arr)
+    
+    leftToRight = 0
+    rightToLeft = 0
+    
+    for i in range(0, n):
+        for j in range(0, n):
+            if i == j:
+                leftToRight += arr[i][j]
+            if ((i + j) == (n - 1)): 
+                rightToLeft += arr[i][j] 
+                
+    return abs(leftToRight - rightToLeft)
+    
+                
+    
+                
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
