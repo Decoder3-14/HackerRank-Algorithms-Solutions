963c59e6649e54f9f3b840152b06ce8e3ab215ba
Ali H. Dulaimi <43238519+Decoder3-14@users.noreply.github.com>
2020-12-18 21:50:38 +0300

    Create BirthdayCandleCakes.py

---
+++ BirthdayCandleCakes.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    hash = {}
+    for can in candles:
+        hash[can] = hash.get(can, 0) + 1
+    
+    max_bag = max(hash.keys())        
+    return hash[max_bag]
+        
+        
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
